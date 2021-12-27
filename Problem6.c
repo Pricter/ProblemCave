9195c9340d3cc735d107153de6afc3bc00b197fd
Veekshith Kodithiyala <kodithiyalaveekshithv2@gmail.com>
2021-12-27 15:33:19 +0530

    Problem 6 done

---
+++ Problem6.c
@@ -0,0 +1,12 @@
+int main() {
+    int sumOfSquares = 0;
+    int squareOfSums = 0;
+    int i = 0;
+    for (i = 1; i <= 100; i++) {
+        sumOfSquares += i * i;
+        squareOfSums += i;
+    }
+    squareOfSums *= squareOfSums;
+    printf("%d\n", squareOfSums - sumOfSquares);
+    return 0;
+}
